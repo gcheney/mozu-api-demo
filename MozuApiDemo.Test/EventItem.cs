2c94c9ce67e266d50ce070b20cb723e6c8726c6f
Glendon Cheney <glendon.cheney@gmail.com>
2017-05-16 14:42:29 -0500

    Create EventItem.cs

---
+++ MozuApiDemo.Test/EventItem.cs
@@ -0,0 +1,29 @@
+using System; 
+
+namespace MozuApiDemoTests
+{
+  public class EventItem
+    {
+        public string Id { get; set; }
+        public string EventId { get; set; }
+        public string EntityId { get; set; }
+        public string Topic { get; set; }
+        public string Status { get; set; }
+        public DateTime QueuedDateTime { get; set; }
+        public DateTime ProcessedDateTime { get; set; }
+
+        public int Retry { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} | {EntityId}~{Topic}~{Status}~{QueuedDateTime}";
+        }
+
+    }
+
+    public enum EventStatus
+    {
+        Processed,
+        Pending,
+        Failed
+    }
+  }
